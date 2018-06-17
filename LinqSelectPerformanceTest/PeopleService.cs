4c4a43935de1bdd347c22d05f765f8d6c4b5502e
Weidaicheng <wdcdavyc@outlook.com>
2018-06-17 14:31:46 +0800

    linq性能测试

---
+++ LinqSelectPerformanceTest/PeopleService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LinqSelectPerformanceTest
+{
+    public class PeopleService
+    {
+        public static IEnumerable<Person> GetPeople(int count)
+        {
+            var people = new List<Person>();
+            for (var i = 0; i < count; i++)
+            {
+                people.Add(new Person()
+                {
+                    Age = 24,
+                    Name = "Jonathan"
+                });
+            }
+
+            return people;
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ LinqSelectPerformanceTest/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static LinqSelectPerformanceTest.PeopleService;
+
+namespace LinqSelectPerformanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 1000000;
+            var people = PeopleService.GetPeople(count);
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("----------热身----------");
+                }
+                else
+                {
+                    Console.WriteLine($"----------第{i}轮----------");
+                }
+                people = PeopleService.GetPeople(count);
+                SelectWithLinq(people);
+
+                people = PeopleService.GetPeople(count);
+                SelectWithForeach(people);
+
+                //people = PeopleService.GetPeople(count);
+                //SelectWithFor(people); 
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<Person> SelectWithLinq(IEnumerable<Person> people)
+        {
+            Console.WriteLine("----------Select With Linq Start----------");
+            var timeStart = DateTime.Now;
+
+            var peopleList = people.Select(p =>
+            {
+                p.Name = "Mr. " + p.Name;
+                return p;
+            }).ToList();
+
+            var timeEnd = DateTime.Now;
+            Console.WriteLine("----------Select With Linq End----------");
+            Console.WriteLine($"\tUse time: {(timeEnd - timeStart).TotalMilliseconds}ms");
+            Console.WriteLine();
+            return peopleList;
+        }
+
+        public static List<Person> SelectWithForeach(IEnumerable<Person> people)
+        {
+            Console.WriteLine("----------Select With Foreach Start----------");
+            var timeStart = DateTime.Now;
+
+            var peopleList = new List<Person>();
+            foreach(var p in people)
+            {
+                p.Name = "Mr. " + p.Name;
+                peopleList.Add(p);
+            }
+
+            var timeEnd = DateTime.Now;
+            Console.WriteLine("----------Select With Foreach End----------");
+            Console.WriteLine($"\tUse time: {(timeEnd - timeStart).TotalMilliseconds}ms");
+            Console.WriteLine();
+            return peopleList;
+        }
+
+        public static List<Person> SelectWithFor(IEnumerable<Person> people)
+        {
+            Console.WriteLine("----------Select With For Start----------");
+            var timeStart = DateTime.Now;
+
+            var peopleList = new List<Person>();
+            for(var i = 0; i < people.Count(); i++)
+            {
+                people.ToList()[i].Name = "Mr. " + people.ToList()[i].Name;
+                peopleList.Add(people.ToList()[i]);
+            }
+
+            var timeEnd = DateTime.Now;
+            Console.WriteLine("----------Select With For End----------");
+            Console.WriteLine($"\tUse time: {(timeEnd - timeStart).TotalMilliseconds}ms");
+            Console.WriteLine();
+            return peopleList;
+        }
+    }
+}
